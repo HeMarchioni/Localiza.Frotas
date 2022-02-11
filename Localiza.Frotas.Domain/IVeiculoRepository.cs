9ff84e82a9ec3d41c7902c3d3532fa32b4e9736d
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-11 14:37:53 -0300

    create CRUD Veiculo

---
+++ Localiza.Frotas.Domain/IVeiculoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localiza.Frotas.Domain
+{
+    public interface IVeiculoRepository
+    {
+        void Add(Veiculo veiculo);
+        void Delete(Veiculo veiculo);
+        void Update(Veiculo veiculo);
+        Veiculo GetById(Guid Id);
+        IEnumerable<Veiculo> GetAll();
+
+
+    }
+}
