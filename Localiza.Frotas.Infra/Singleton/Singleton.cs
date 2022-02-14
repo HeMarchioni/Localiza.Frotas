c3f708e57d43a83939af3a917f9cb56ffdbcd211
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-02-14 00:36:37 -0300

    correção de Bug

--- Localiza.Frotas.Infra/Singleton/Singleton.cs
+++ Localiza.Frotas.Infra/Singleton/Singleton.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Localiza.Frotas.Infra.Singleton
+﻿namespace Localiza.Frotas.Infra.Singleton
 {
 
 
@@ -37,4 +31,5 @@ namespace Localiza.Frotas.Infra.Singleton
 
 
         }
+    }
 }
